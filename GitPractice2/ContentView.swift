912ca3b8af12224b8602b1be5f1d3e63e58edb98
Ashley <anm38060@gmail.com>
2025-06-04 04:53:14 -0500

    Revised the home layout

--- GitPractice2/ContentView.swift
+++ GitPractice2/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Ashley!")
+                .fontWeight(.bold)
+            Text("Goodbye, Ashley")
         }
         .padding()
     }
